2cf4a51b8b73cd68b4d0c8c55ae2ae52f91e2408
Francois Laplaud <el.francesco@gmail.com>
2015-02-27 08:54:34 +0000

    first working version

--- install/install-supervisor.sh
+++ install/install-supervisor.sh
@@ -6,9 +6,9 @@ VENVNAME=venv
 VENVREQUIREMENTS=$PROJECTPATH/install/requirements.txt
 
 echo "Prérequis : "
-echo "- installer supervisor et python-virtualenv"
+echo "- sudo apt-get install supervisor python-virtualenv"
 echo "- installer les outils de dev pour améliorer les performances de tornado"
-
+echo "- sudo apt-get install build-essential python-dev"
 echo "###############################"
 echo "Ce script va créer le fichier de configuration supervisor qu'il faudra ensuite placer dans /etc/supervisor/conf.d (sur debian)"
 echo "Vous allez devois préciser le nom du compte utilisateur qui exécutera le script python."
@@ -23,22 +23,20 @@ echo ""
 echo "Supervisor lancera le $PROJECT_PATH/Controller.py avec l'utilisateur $USER"
 echo "Création du fichier de configuration de $PROJECTNAME"
 
-echo << EOF > $PROJECTNAME.conf
+cat << EOF > $PROJECTNAME.conf
 [program:$PROJECTNAME]
-command=$PROJECT_PATH/$VENVNAME/bin/python $PROJECT_PATH/Controller.py
+command=$PROJECTPATH/$VENVNAME/bin/python $PROJECTPATH/Controller.py
 user=$USER
 
 EOF
-
+ 
 echo "##############################"
 echo "Le fichier $PROJECTNAME.conf vient d'être généré. Vous devrez le copier dans le dossier de configuration de supervisor."
 echo "##############################"
-pushd ./
+#pushd ./
 cd ..
 virtualenv $VENVNAME
 echo "Le Virtualenv $VENVNAME a été créé"
 echo "Entrée dans le virtualenv"
-source $VENVNAME/bin/activate
-echo "Installation des dépendances"
-pip install -r $VENVREQUIREMENTS
+bash -c ". $VENVNAME/bin/activate; pip install -r $VENVREQUIREMENTS;"
 
